9100057c9bc318b42ceb53509f83ee44441166d0
Vlada Emanuel <emanuel@angularsoft.ro>
2019-08-09 17:32:01 +0300

    update image

--- Componente/react/App.js
+++ Componente/react/App.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 
 import '../../Client/development/css/style.css';
 
+const image = require ('../../Client/development/images/misc/ralogo_monogram.png');
+
 class App extends React.Component {
 	render() { 
 		return (
-			<div className="App">test 2</div>
+			<div className="App">test 2 <img src={image}/></div>
 			);
 	}
 } 
